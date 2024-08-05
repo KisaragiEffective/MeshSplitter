3ab23e3416f37035aca819710ca83c485d4fbd64
Kisaragi Marine <kisaragi.effective@gmail.com>
2024-08-05 18:01:01 +0900

    refactor: reflow

--- Editor/MeshSplitter.cs
+++ Editor/MeshSplitter.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEditor;
 using UnityEditor.UIElements;
 using UnityEngine.UIElements;
@@ -29,7 +25,7 @@ namespace KiriMeshSplitter
 				}
 			};
 			rootVisualElement.Add(targetMeshField);
-			
+			var targetMeshFieldProxy = new TypeSafeObjectField<SkinnedMeshRenderer>(targetMeshField);
 			
 			var quad = new ObjectField("Quad")
 			{
@@ -40,8 +36,9 @@ namespace KiriMeshSplitter
 				}
 			};
 			rootVisualElement.Add(quad);
+			var quadFieldProxy = new TypeSafeObjectField<MeshFilter>(quad);
 			
-			var splitByMaterials = new Button(() => { SplitByMaterials(targetMeshField.value as SkinnedMeshRenderer); })
+			var splitByMaterials = new Button(() => { BusinessLogic.SplitByMaterials(targetMeshFieldProxy.Value); })
 				{
 					style =
 					{
@@ -50,9 +47,12 @@ namespace KiriMeshSplitter
 				};
 			rootVisualElement.Add(splitByMaterials);
 			splitByMaterials.Add(new Label("split by materials"));
-			splitByMaterials.style.display = targetMeshField.value != null ? DisplayStyle.Flex : DisplayStyle.None;
+			splitByMaterials.style.display = targetMeshFieldProxy.Value != null ? DisplayStyle.Flex : DisplayStyle.None;
 			
-			var splitByQuad = new Button(() => { SplitByQuad(targetMeshField.value as SkinnedMeshRenderer, quad.value as MeshFilter); })
+			var splitByQuad = new Button(() =>
+			{
+				BusinessLogic.SplitByQuad(targetMeshFieldProxy.Value, quadFieldProxy.Value);
+			})
 				{
 					style =
 					{
@@ -61,9 +61,9 @@ namespace KiriMeshSplitter
 				};
 			rootVisualElement.Add(splitByQuad);
 			splitByQuad.Add(new Label("split by quad"));
-			splitByQuad.style.display = quad.value != null ? DisplayStyle.Flex : DisplayStyle.None;
+			splitByQuad.style.display = quadFieldProxy.Value != null ? DisplayStyle.Flex : DisplayStyle.None;
 			
-			targetMeshField.RegisterValueChangedCallback(ev =>
+			targetMeshFieldProxy.ObjectField.RegisterValueChangedCallback(ev =>
 			{
 				var visibleSplitByMaterial = ev.newValue != null;
 				splitByMaterials.style.display = visibleSplitByMaterial ? DisplayStyle.Flex : DisplayStyle.None;
@@ -72,7 +72,7 @@ namespace KiriMeshSplitter
 			{
 				if (ev.newValue != null && (ev.newValue as MeshFilter)!.name != "Quad")
 				{
-					quad.value = null;
+					quadFieldProxy.Value = null;
 				}
 			});
 			quad.RegisterValueChangedCallback(ev =>
@@ -80,137 +80,6 @@ namespace KiriMeshSplitter
 				var visibleSplitByQuad = ev.newValue != null;
 				splitByQuad.style.display = visibleSplitByQuad ? DisplayStyle.Flex : DisplayStyle.None;
 			});
-
-		}
-
-		private static void SplitByMaterials (SkinnedMeshRenderer meshRenderer)
-		{
-			var submeshDir = GetSubmeshPath (meshRenderer);
-			CreateFolder (submeshDir);
-
-			for (var i = 0; i < meshRenderer.sharedMesh.subMeshCount; i++) {
-				SplitSubmesh (meshRenderer, i, submeshDir);
-			}
-
-			meshRenderer.gameObject.SetActive (false);
-		}
-
-		private static string GetSubmeshPath (SkinnedMeshRenderer meshRenderer)
-		{
-			var meshPath = AssetDatabase.GetAssetPath (meshRenderer.sharedMesh);
-			var baseDir = Path.GetDirectoryName (meshPath);
-
-			if (baseDir.EndsWith ("Submeshes")) {
-				return baseDir;
-			} else {
-				return Path.Combine (baseDir, "Submeshes");
-			}
-		}
-
-		private static void CreateFolder (string path)
-		{
-			if (AssetDatabase.IsValidFolder (path)) {
-				return;
-			}
-
-			var parent = Path.GetDirectoryName (path);
-			var dirname = Path.GetFileName (path);
-
-			if (!AssetDatabase.IsValidFolder (parent)) {
-				CreateFolder (parent);
-			}
-
-			AssetDatabase.CreateFolder (parent, dirname);
-		}
-
-		private static void SplitSubmesh (SkinnedMeshRenderer meshRenderer, int index, string submeshDir)
-		{
-			var materialName = meshRenderer.sharedMaterials [index].name;
-			var meshName = meshRenderer.gameObject.name + "_" + materialName;
-			var tris = new List<int>();
-			meshRenderer.sharedMesh.GetTriangles(tris, index);
-			var triangles = new List<List<int>> { tris };
-
-			var newMeshRenderer = CreateNewMesh (meshRenderer, triangles, submeshDir, meshName);
-			newMeshRenderer.sharedMaterials = new[] { meshRenderer.sharedMaterials [index] };
-		}
-
-		private static GameObject CloneObject (GameObject gameObject)
-		{
-			return Instantiate (gameObject, gameObject.transform.parent);
-		}
-
-		private static void SplitByQuad (SkinnedMeshRenderer meshRenderer, MeshFilter meshFilter)
-		{
-			var plane = CreatePlane (meshFilter);
-			var mesh = meshRenderer.sharedMesh;
-			var matrix = meshRenderer.transform.localToWorldMatrix;
-
-			var meshName = meshRenderer.gameObject.name;
-			var submeshDir = GetSubmeshPath (meshRenderer);
-			CreateFolder (submeshDir);
-
-			var triA = new List<List<int>> ();
-			var triB = new List<List<int>> ();
-
-			// 取得するたびにいちいちコピーが走り、遅いのでローカル変数に突っ込んでそれを読む
-			var tris = mesh.vertices;
-
-			for (var subMeshIndex = 0; subMeshIndex < mesh.subMeshCount; subMeshIndex++) {
-				var triangles = mesh.GetTriangles (subMeshIndex);
-				triA.Add (new List<int> ());
-				triB.Add (new List<int> ());
-
-				for (var triangleIndex = 0; triangleIndex < triangles.Length; triangleIndex += 3) {
-					var triangle = triangles.Skip (triangleIndex).Take (3).ToList();
-					var side = triangle.Any(n => plane.GetSide(matrix.MultiplyPoint(tris[n])));
-
-					if (side) {
-						triA [subMeshIndex].AddRange (triangle);
-					} else {
-						triB [subMeshIndex].AddRange (triangle);
-					}
-
-					if (triangleIndex % 30 == 0) {
-						EditorUtility.DisplayProgressBar (
-							"処理中", 
-							string.Format ("submesh:{0}/{1}, triangles:{2}/{3}", subMeshIndex, mesh.subMeshCount, triangleIndex, triangles.Length),
-							(float)triangleIndex / triangles.Length
-						);
-					}
-				}
-			}
-
-			CreateNewMesh (meshRenderer, triA, submeshDir, meshName + "_a");
-			CreateNewMesh (meshRenderer, triB, submeshDir, meshName + "_b");
-			meshRenderer.gameObject.SetActive (false);
-
-			EditorUtility.ClearProgressBar (); 
-		}
-
-		private static SkinnedMeshRenderer CreateNewMesh (SkinnedMeshRenderer original, List<List<int>> triangles, string dirname, string name)
-		{
-			var gameObject = CloneObject (original.gameObject);
-			gameObject.name = name;
-			var meshRenderer = gameObject.GetComponent<SkinnedMeshRenderer>();
-			var mesh = Instantiate (meshRenderer.sharedMesh);
-
-			mesh.subMeshCount = triangles.Count; 
-			for (var i = 0; i < triangles.Count; i++) {
-				mesh.SetTriangles (triangles [i], i);
-			}
-			AssetDatabase.CreateAsset (mesh, Path.Combine (dirname, name + ".asset"));
-			meshRenderer.sharedMesh = mesh;
-
-			return meshRenderer;
-		}
-
-		private static Plane CreatePlane (MeshFilter meshFilter)
-		{
-			var matrix = meshFilter.transform.localToWorldMatrix;
-			var mesh = meshFilter.sharedMesh;
-			var vertices = mesh.triangles.Take (3).Select (n => matrix.MultiplyPoint (mesh.vertices [n])).ToArray ();
-			return new Plane (vertices [0], vertices [1], vertices [2]);
 		}
 	}
 }
